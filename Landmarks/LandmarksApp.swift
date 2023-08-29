a9e9ed2cb7f468a06c4c44e3d6b14729ed550ae2
Luis Bandeira <luisbandeira@WISER-001946.local>
2023-08-29 08:17:41 -0300

    first commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Luis Bandeira on 24/08/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
